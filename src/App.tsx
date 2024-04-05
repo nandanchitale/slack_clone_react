790670b8360ff258e7dffdb669541bc19a56fd3f
Nandan <nandan.chitale@datastack.tech>
2024-04-05 13:21:32 +0530

    Day 2: Google Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
@@ -6,25 +6,32 @@ import {
   BrowserRouter as Router, Routes, Route
 } from "react-router-dom";
 import Chat from './Components/Chat/Chat';
-
+import Login from './Components/Login/Login';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
       <Router>
-        <Header />
-        <div className="app_body">
-          <Sidebar />
-          <Routes>
-            <Route path="/room/:roomId" element={<Chat />}>
-              {/* <Chat /> */}
-            </Route>
-            <Route path="/" element={<h1>Welcome</h1>}>
-              {/* <Chat /> */}
-            </Route>
-          </Routes>
-          {/* React Router -> Chat Screen */}
-        </div>
+        {!user ? (<Login />) : (
+          <>
+            <Header />
+            <div className="app_body">
+              <Sidebar />
+              <Routes>
+                <Route path="/room/:roomId" element={<Chat />}>
+                  {/* <Chat /> */}
+                </Route>
+                <Route path="/" element={<h1>Welcome</h1>}>
+                  {/* <Chat /> */}
+                </Route>
+              </Routes>
+              {/* React Router -> Chat Screen */}
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
